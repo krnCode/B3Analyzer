42fe43cba82fa082575fe8cc5b168b1c84df1773
krn <contadorkrn@gmail.com>
2024-02-11 14:17:18 -0300

    Added: support for many file uploads / Changes: data cleaning and datatype changes

--- main/app.py
+++ main/app.py
@@ -1,16 +1,40 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 
+
+dfs = []
+
+
+def create_dataframe(files, dfs):
+    if files:
+        st.markdown("### Extrato consolidado:")
+        for file in files:
+            df = pd.read_excel(io=file)
+            dfs.append(df)
+
+        df = pd.concat(dfs, ignore_index=True)
+
+        return df
+
+    else:
+        st.markdown("# Faça o upload dos seus extratos na tela lateral 👈")
+
+
 with st.sidebar:
-    file = st.file_uploader(label="Envie o extrato em excel (extensão .xlsx)")
-
-
-if file:
-    excel_file = pd.read_excel(
-        io=file,
+    files = st.file_uploader(
+        label="Envie os extratos da B3 em excel (extensão .xlsx)",
+        accept_multiple_files=True,
     )
-    st.markdown("## Seu extrato:")
-    st.dataframe(data=excel_file, use_container_width=True)
 
-else:
-    st.markdown("### Faça o upload do seu extrato na tela lateral")
+df = create_dataframe(files, dfs)
+
+if df is not None:
+    # Clean "-" strings from numeric columns and display them as a single dataframe
+    df["Preço unitário"] = df["Preço unitário"].replace("-", 0)
+    df["Valor da Operação"] = df["Valor da Operação"].replace("-", 0)
+    st.dataframe(data=df, use_container_width=True)
+
+    # Convert the "data" column as a datetime type
+    # Did not converted before displaying with "st.dataframe()"because the string format is easier to read
+    df["Data"] = pd.to_datetime(df["Data"])
