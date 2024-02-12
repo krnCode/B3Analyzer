0f4153e00da8278f0ab42f017a0b1f939ea3f815
krn <contadorkrn@gmail.com>
2024-02-12 10:55:43 -0300

    Added: columns separating incoming and outgoing entries / Changed: cleaned data to change outgoing entries to negative number, commented out the conversion of datetime

--- main/app.py
+++ main/app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import numpy as np
 from io import BytesIO
 
 # APP PAGE CONFIG
@@ -25,7 +26,7 @@ def convert_to_excel(df: pd.DataFrame):
 
 def create_dataframe(files, dfs):
     if files:
-        st.markdown("### Extrato consolidado:")
+        st.markdown("### Extrato consolidado")
         for file in files:
             df = pd.read_excel(io=file)
             dfs.append(df)
@@ -46,6 +47,8 @@ with st.sidebar:
         accept_multiple_files=True,
     )
 
+if "files" not in st.session_state:
+    st.session_state.files = files
 
 df = create_dataframe(files, dfs)
 
@@ -58,6 +61,20 @@ if df is not None:
     # Clean "-" strings from numeric columns and display them as a single dataframe
     df["Preço unitário"] = df["Preço unitário"].replace("-", 0)
     df["Valor da Operação"] = df["Valor da Operação"].replace("-", 0)
+
+    # Changing outgoing entries to a negative number
+    df["Valor da Operação"] = np.where(
+        df["Entrada/Saída"] == "Debito",
+        df["Valor da Operação"] * -1,
+        df["Valor da Operação"],
+    )
+
+    df["Quantidade"] = np.where(
+        df["Entrada/Saída"] == "Debito",
+        df["Quantidade"] * -1,
+        df["Quantidade"],
+    )
+
     st.dataframe(data=df, use_container_width=True)
 
     # Convert dataframe in excel format for download
@@ -69,6 +86,18 @@ if df is not None:
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     )
 
+    # Filter "entradas" and "saídas"
+    df_in = df[df["Entrada/Saída"].values == "Credito"]
+    df_out = df[df["Entrada/Saída"].values == "Debito"]
+
+    col1, col2 = st.columns([1, 1])
+
+    col1.subheader("Entradas")
+    col1.dataframe(df_in)
+
+    col2.subheader("Saídas")
+    col2.dataframe(df_out)
+
     # Convert the "data" column as a datetime datatype
     # Did not converted before displaying with "st.dataframe()" because the unformatted string is easier to read
-    df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y")
+    # df["Data"] = pd.to_datetime(df["Data"], format="%d/%m/%Y")
