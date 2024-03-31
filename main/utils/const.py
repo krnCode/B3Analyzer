d25e930b8d2cd408aa971192cdaf1f94e8779e90
krn <contadorkrn@gmail.com>
2024-03-31 11:32:25 -0300

    Criado arquivo para guardar constantes

---
+++ main/utils/const.py
@@ -0,0 +1,24 @@
+# Descrição das movimentações a serem consideradas como rendimento
+TIPOS_DE_RENDIMENTO: list = [
+    "Amortização",
+    "Dividendo",
+    "Juros",
+    "Juros Sobre Capital Próprio",
+    "Rendimento",
+]
+
+# Meses em ordem cronológica
+MESES: list = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+]
