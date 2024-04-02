667325b96f92924dbfb08edbcada04334dc1420b
krn <contadorkrn@gmail.com>
2024-04-01 21:34:30 -0300

    Incluído opção para selecionar ano e mês nos filtros; incluído extratos e download para excel.

--- app.py
+++ app.py
@@ -52,18 +52,38 @@ if extratos:
     with st.sidebar:
         st.markdown("Filtros:")
 
+        col1, col2 = st.columns(spec=[1, 1])
+
+        with col1:
+            ano = st.multiselect(
+                label="Ano",
+                options=df["Ano"].sort_values(ascending=True).unique(),
+                default=None,
+                placeholder="",
+            )
+
+        with col2:
+            mes = st.multiselect(
+                label="Mes",
+                options=df["Mes"].sort_values(ascending=True).unique(),
+                default=None,
+                placeholder="",
+            )
+
         movimentação = st.multiselect(
             label="Movimentação",
             options=df["Movimentação"].sort_values(ascending=True).unique(),
             default=None,
             placeholder="",
         )
+
         ticker = st.multiselect(
             label="Ticker",
             options=df["Ticker"].sort_values(ascending=True).unique(),
             default=None,
             placeholder="",
         )
+
         corretora = st.multiselect(
             label="Corretora",
             options=df["Instituição"].sort_values(ascending=True).unique(),
@@ -80,6 +100,12 @@ if extratos:
     # TODO: incluir mais opções de filtro (período, mes, ano)
     # Lógica dos filtros
     query = []
+    if ano:
+        query.append(f"Ano == {ano}")
+
+    if mes:
+        query.append(f"Mes == {mes}")
+
     if movimentação:
         query.append(f"Movimentação == {movimentação}")
 
@@ -97,18 +123,47 @@ if extratos:
 
     st.markdown("# Análise dos Investimentos")
 
-    metricas, extratos, analises = st.tabs(["Métricas", "Extratos", "Análises"])
+    metricas, extratos, ativos = st.tabs(["Métricas", "Extratos", "Ativos"])
 
     # TODO: incluir métricas
     with metricas:
         pass
 
-    # TODO: incluir extratos
     with extratos:
+        st.markdown("#### Extrato Consolidado")
         st.dataframe(data=df_filtered)
+        st.download_button(
+            label="Exportar Excel",
+            data=converter_para_excel(df_filtered),
+            file_name="b3_extrato_consolidado.xlsx",
+            key="b3_extrato_consolidado",
+        )
+        st.markdown("---")
+
+        st.markdown("#### Entradas/Saídas")
+        entradas = separar_entradas(df=df_filtered)
+        saidas = separar_saidas(df=df_filtered)
+
+        col1, col2 = st.columns(spec=[1, 1])
+
+        with col1:
+            st.markdown("##### Entradas")
+            st.dataframe(data=entradas, use_container_width=True)
+
+        with col2:
+            st.markdown("##### Saídas")
+            st.dataframe(data=saidas, use_container_width=True)
+
+        st.download_button(
+            label="Exportar Excel",
+            data=converter_para_excel_varias_planilhas(dfs=[entradas, saidas]),
+            file_name="b3_extrato_entradas_saidas.xlsx",
+            key="b3_extrato_entradas_saidas",
+        )
+        st.markdown("---")
 
     # TODO: incluir análises
-    with analises:
+    with ativos:
         pass
 
 
