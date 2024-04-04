d253d2ebf9b0339684c2f59af71fb155c0241467
krn <contadorkrn@gmail.com>
2024-04-03 22:14:25 -0300

    Alterado import da classe

--- libs/Rendimentos.py
+++ libs/Rendimentos.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from dataclasses import dataclass
-from data_cleaning import *
+from libs.data_cleaning import *
 
 # PANDAS CONFIG
 # -----------------------------
