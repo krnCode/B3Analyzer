5a5f1fa07aa4fdcd2bc2b3825579704ca600d64a
krn <contadorkrn@gmail.com>
2024-04-11 20:24:13 -0300

    Atualizado código para não ter mais o aviso SettingWithCopyWarning

--- libs/Futuros.py
+++ libs/Futuros.py
@@ -13,8 +13,8 @@ class Futuros:
 
     def pegar_somente_futuros(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df[df["Descrição Ticker"].str.contains("WDO|WIN")]
-        df["Descrição Ticker"] = df["Descrição Ticker"] + " - " + df["Ticker"]
-        df["Ticker"] = df["Descrição Ticker"].str[:6]
+        df.loc[:, "Descrição Ticker"] = df["Descrição Ticker"] + " - " + df["Ticker"]
+        df.loc[:, "Ticker"] = df["Descrição Ticker"].str[:6]
         df["Preço unitário"] = np.where(
             df["Movimentação"] == "Compra",
             df["Preço unitário"] * -1,
